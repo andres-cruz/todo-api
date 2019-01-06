cecddf59c448134d6aea37f7c218e825d9e0fc45
Andres <andrescruz.tech@gmail.com>
2019-01-06 17:31:50 -0300

    Update config to use JSON file

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -12,23 +12,34 @@ const userOneId = new ObjectID();
 const userTwoId = new ObjectID();
 
 // array users
-const users = [{
+const users = [
+  {
     _id: userOneId,
-    email: 'andrew@example.com',
-    password: 'userOnePass',
-    tokens: [{
-        access: 'auth',
-        token: jwt.sign({ _id: userOneId, access: 'auth' }, 'abc123').toString()
-    }]
-}, {
+    email: "andrew@example.com",
+    password: "userOnePass",
+    tokens: [
+      {
+        access: "auth",
+        token: jwt
+          .sign({ _id: userOneId, access: "auth" }, process.env.JWT_SECRET)
+          .toString()
+      }
+    ]
+  },
+  {
     _id: userTwoId,
-    email: 'jen@example.com',
-    password: 'userTwoPass',
-        tokens: [{
-            access: 'auth',
-            token: jwt.sign({ _id: userTwoId, access: 'auth' }, 'abc123').toString()
-        }]
-}];
+    email: "jen@example.com",
+    password: "userTwoPass",
+    tokens: [
+      {
+        access: "auth",
+        token: jwt
+          .sign({ _id: userTwoId, access: "auth" }, process.env.JWT_SECRET)
+          .toString()
+      }
+    ]
+  }
+];
 
 // array to-do
 const todos = [
